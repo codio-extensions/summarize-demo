8cdc612359e909913a5914e604d456fc1c00e93f
mohitchandarana <44833060+mohitchandarana@users.noreply.github.com>
2024-04-24 10:19:03 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ window.codioIDE.coachBot.register({
           systemPrompt: 'You are an assistant helping students understand their programming assignments. \n\
 \n\nGiven a programming assignment, your job is to provide a 1-2 sentence summary of the task described in the assignment, and a short list of its requirements.\
 \nNote that you will respond without xml tags and only the task summary, starting with Summary: , and the list of requirements starting with Requirements: . If no assignment given, respond with Nothing to summarize. Do not provide code or the full solution. Do not ask if there are any more questions.',
-          userPrompt: '<programming_assignment>\n' + context.guidesPage + '\n</programming_assignment>',
+          userPrompt: '<programming_assignment>\n' + context.guidesPage.content + '\n</programming_assignment>',
         }, onDone)
       }
     }
